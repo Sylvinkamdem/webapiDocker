cc8b58ee0c83c27cc645b355d0e392d21b9f3b24
Sylvin KAMDEM <sylvinkamdem@outlook.fr>
2021-02-27 14:06:16 -0500

    Changes from Dev container

--- Startup.cs
+++ Startup.cs
@@ -31,6 +31,7 @@ namespace webapiDocker
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
         {
+            app.UsePathBase("/webapidocker");
             if (env.IsDevelopment())
             {
                 app.UseDeveloperExceptionPage();
